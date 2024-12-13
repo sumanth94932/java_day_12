98af1cde02fa2e3d3098c7637c34ac25083ee9f5
sumanth94932 <sumanthreddyr126@gmail.com>
2024-12-13 16:00:41 +0530

    Add files via upload

---
+++ unionnn.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+class unionnn{
+    public static void main(String [] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr1[]=new int[n];
+        for(int i=0;i<n;i++){
+            arr1[i]=s.nextInt();
+        }
+         int m=s.nextInt();
+        int arr2[]=new int[n];
+        for(int i=0;i<m;i++){
+            arr2[i]=s.nextInt();
+        }
+        Set<Integer> a=new HashSet<>();
+        Set<Integer> b=new HashSet<>();
+        for(int s1:arr1)
+            a.add(s1);
+        for(int s2:arr2)
+            b.add(s2);
+        System.out.println(a);
+        System.out.println(b);
+        Set<Integer> c=new HashSet<>(a);
+        c.addAll(b);
+        System.out.println(c);
+    }
+}/* 3
+4 7 9 
+3 
+4 9 1 
+[4, 7, 9]   
+[1, 4, 9]   
+[1, 4, 7, 9]  */
